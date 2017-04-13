e5215b9e626a577d4a60e79c7ba1b99bb6e82253
zhaoxubo <1094311061@qq.com>
2017-04-13 14:26:11 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by 赵旭波 on 2017/4/13.
+//  Copyright © 2017年 赵旭波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
